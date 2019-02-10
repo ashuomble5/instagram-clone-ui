323d417a1c398c2012ca797aa0b4dc47f12b0e64
ashuomble5 <ashuomble5@gmail.com>
2019-02-10 21:08:59 +0530

    with stories section - unfinished

--- src/component/App.js
+++ src/component/App.js
@@ -16,6 +16,15 @@ class App extends React.Component {
               <Posts />
             </div>
             <div className="six wide column">
+              <div className="content">
+                {/* <div className="right floated meta">14h</div> */}
+                <img
+                  className="ui circle avatar image"
+                  alt="user"
+                  src="https://instagram.fbom11-1.fna.fbcdn.net/vp/b4c6f3c97c20bc07e8e527006552290d/5CEEB1D8/t51.2885-19/s150x150/46959381_745323512511418_5824924179723452416_n.jpg?_nc_ht=instagram.fbom11-1.fna.fbcdn.net"
+                />{" "}
+                <strong>evilsempire</strong>
+              </div>
               <Stories />
               <Suggestions />
             </div>
--- src/component/Stories.js
+++ src/component/Stories.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Stories = () => {
-    return <div>Stories</div>
-}
+  return (
+    <div className="ui segment">
+      <div className="meta">Stories</div>
+      <div className="ui relaxed divided list">
+        <div className="item">
+          <img
+            className="ui avatar image"
+            alt="user"
+            src="https://semantic-ui.com/images/avatar/large/elliot.jpg"
+          />
+          <div class="content">
+            <a class=" text-strong" style={{ color: "black !important" }}>
+              Utkarsh Joshi
+            </a>
+            <div class="description">10 mins ago</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Stories;
