7c5e08f192899ceb5329dcd4c366b027ea443215
ashuomble5 <ashuomble5@gmail.com>
2019-02-19 09:23:27 +0530

    Added suggestions component

--- src/component/Suggestions.js
+++ src/component/Suggestions.js
@@ -1,7 +1,32 @@
 import React from "react";
 
 const Suggestions = () => {
-  return <div>Suggestions</div>;
+  return (
+    <div className="ui segment">
+      <div className="ui float-left left meta">Suggestions For You</div>
+      <div className="ui float-right right floated">See all</div>
+      <br />
+      <div className="ui relaxed divided list">
+        <div className="item">
+          <img
+            className="ui avatar image"
+            alt="user"
+            src="https://semantic-ui.com/images/avatar/large/elliot.jpg"
+          />
+          <div class="content">
+            <a class=" text-strong" style={{ color: "black !important" }}>
+              Utkarsh Joshi
+            </a>
+
+            <div class="description">10 mins ago</div>
+          </div>
+          <div className="ui float-right right floated follow-color">
+            <b>Follow</b>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Suggestions;
